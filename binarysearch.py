8c43b3733ee9b7b62b926403d3e2bc0eedc164f8
Akshar Raaj <akshar@agiliq.com>
2012-11-24 12:34:10 +0530

    binarysearch returns index of found element else -1

--- binarysearch.py
+++ binarysearch.py
@@ -1,4 +1,4 @@
-def binarysearch(slist, value):
+"""def binarysearch(slist, value, last_index):
     length = len(slist)
     if(length==0):
         return -1
@@ -6,15 +6,31 @@ def binarysearch(slist, value):
         return 0 if slist[0]==value else -1
     index = length/2
     if(slist[index]==value):
-        return 1
+        return last_index 
     elif(value<slist[index]):
-        return binarysearch(slist[:index], value)
+        if(length%2==0):
+            last_index = last_index-length/2
+        else:
+            last_index = last_index-1-length/2
+        return binarysearch(slist[:index], value, last_index)
     else:
-        return binarysearch(slist[index:], value)
+        return binarysearch(slist[index:], value, last_index)"""
 
-l = [1, 2, 3, 4, 5]
-found = binarysearch(l, 0)
-if found>=0:
-    print "Element found"
-else:
-    print "Element not found"
+
+not_found = -1
+def binarysearch(slist, value, left, right):
+    while(left<=right):
+        mid = (left+right)/2
+        if slist[mid]==value:
+            return mid
+        elif value<slist[mid]:
+            right = mid-1
+            return binarysearch(slist, value, left, right)
+        else:
+            left = mid+1
+            return binarysearch(slist, value, left, right)
+    return -1
+
+l = [1, 2, 3]
+found = binarysearch(l, 5, 0, len(l)-1)
+print found
