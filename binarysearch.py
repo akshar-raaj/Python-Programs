56c6f0fda97c470ec7688b4dc61b04b7b3415b21
Akshar Raaj <akshar@agiliq.com>
2012-11-24 17:19:22 +0530

    Quick sort

--- binarysearch.py
+++ binarysearch.py
@@ -18,6 +18,34 @@
 
 
 not_found = -1
+import unittest
+class TestBinS(unittest.TestCase):
+    def test_empty(self):
+        lst = []
+        key = 0
+        self.assertEquals(binsearch(lst, 0,), -1)
+
+    """def test_equal(self):
+       lst = [0, 0, 0, 0]
+       self.assertEquals(binsearch(lst, 0,), 0)"""
+
+    def test_negs(self):
+       lst = [-10, -1, 0, 1 , 10]
+       self.assertEquals(binsearch(lst, 0,), 2)
+
+    def test_randoms(self):
+       import random
+       size = random.randint(0, 10)
+       lst = sorted([random.randint(0, 10) for i in range(size)])
+       if lst: #lst can be an empty list and then lst[0] will be an error
+           self.assertEquals(binsearch(lst, lst[0],), 0)
+
+
+def binsearch(slist, value, left=0, right=None):
+    if not right:
+        right = len(slist)-1
+    return binarysearch(slist, value, left, right)
+
 def binarysearch(slist, value, left, right):
     while(left<=right):
         mid = (left+right)/2
@@ -31,6 +59,5 @@ def binarysearch(slist, value, left, right):
             return binarysearch(slist, value, left, right)
     return -1
 
-l = [1, 2, 3]
-found = binarysearch(l, 5, 0, len(l)-1)
-print found
+if __name__=="__main__":
+    unittest.main()
---
+++ quicksort.py
@@ -0,0 +1,54 @@
+#We will make sure to not pass an empty list here
+
+def concatenate(left_list, pivot, right_list):
+    left_list.extend(pivot)
+    left_list.extend(right_list)
+    return left_list
+
+def quicksort(li):
+    if len(li)==1 or len(li)==0:
+        return li
+    pivot = li[0]
+    left_list= []
+    right_list = []
+    for i in range(1, len(li)):
+        if(li[i]<=pivot):
+            left_list.append(li[i])
+        else:
+            right_list.append(li[i])
+    return concatenate(quicksort(left_list), [pivot], quicksort(right_list))
+
+def qs(li):
+    return quicksort(li)
+
+import unittest
+import random
+class TestQuickSort(unittest.TestCase):
+    def test_empty(self):
+        print "Test ran"
+        lst = []
+        ls = qs(lst)
+        self.assertEqual(len(ls), 0)
+
+    def test_one(self):
+        lst = [1]
+        ls = qs(lst)
+        self.assertEqual(len(ls), 1)
+        self.assertEqual(ls[0], 1)
+
+    def test_equal(self):
+        lst = [1,1]
+        ls = qs(lst)
+        self.assertEqual(len(ls), 2)
+        self.assertEqual(ls[1], 1)
+
+    def test_random(self):
+        size = random.randint(0, 10)
+        lst = [random.randint(0, 100) for i in range(size)]
+        ls = qs(lst)
+        self.assertEqual(len(ls), size)
+        lst.sort()
+        self.assertEqual(ls[0], lst[0])
+
+if __name__=="__main__":
+    unittest.main()
