f38d6bb5e20bddb18b4018b8e746aac8e9022417
Akshar Raaj <akshar@agiliq.com>
2012-12-09 23:07:40 +0530

    permutations() return a list of all the permutations, in the same format as itertools.permutations

--- permutations.py
+++ permutations.py
@@ -1,15 +1,16 @@
 def permutation(li):
     stack = []
     if len(li)==1:
-        return li
+        return [li]
     else:
         for i, each in enumerate(li):
             left_list = li[:i]
             right_list = li[i+1:]
-            left_list.extend(right_list)
-            result = permutation(left_list)
+            left_list = left_list + right_list
+            result = [e for e in permutation(left_list)]
             for res in result:
-                stack.append(each+res)
+                res.insert(0, each)
+                stack.append(res)
         return stack
 
 result = permutation(['a', 'b', 'c'])
